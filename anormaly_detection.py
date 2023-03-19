f7ea1297bf40a2b2ca929b8af0a9b6ca3bc4205d
ashlynzzzz <kxinzhao@umich.edu>
2023-03-19 15:47:06 -0400

    Update anormaly_detection.py

---
+++ anormaly_detection.py
@@ -0,0 +1,22 @@
+import numpy as np
+from mdwsvm import mdwsvm
+from mdwsvm_ad import mdwsvm_ad
+from hybrid import hybrid
+
+#TODO: Data preprocessing
+# 1. load data
+# 2. extract training data and test data
+# 3. change lower-case classes as one label 
+X_train = 0
+y_train = 0
+X_test = 0
+y_test = 0
+
+#TODO: Compare model
+# use cross validation to choose hyperparameter
+# mdwsvm
+# mdwsvm_ad
+# hybrid: use the same hyperparameter chosen in mdwsvm
+
+#TODO:
+# show result
