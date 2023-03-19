fa3d72c249e29395a01731a2ea6b87be80c6e0e7
ashlynzzzz <kxinzhao@umich.edu>
2023-03-19 15:33:23 -0400

    Update msvm.py

--- msvm.py
+++ msvm.py
@@ -6,7 +6,7 @@ def msvm(X, W, C):
     This function aims to reimplement MSVM to do multiclass classification
 
     Input: 
-        X:  ata Matrix of interest (p by n) where n is the number of training samples and p is the number of features
+        X:  Data Matrix of interest (p by n) where n is the number of training samples and p is the number of features
         W:  vertices matrix for all classes
         C:  constraint hyperparameter on B which is the coefficient in f
         
