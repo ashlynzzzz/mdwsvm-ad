4505caa1b53b38c94e829c532a9a8157b03d10c6
ashlynzzzz <kxinzhao@umich.edu>
2023-03-19 13:40:35 -0400

    Create one_class_svm.py

---
+++ one_class_svm.py
@@ -0,0 +1,27 @@
+import numpy as np
+import cvxpy as cp
+
+def one_class_svm(X, v, K = lambda u, v: np.exp(-np.linalg.norm(u - v)**2/2)):
+    '''
+    This function aims to do one-class svm and label anomalous vectors as +1
+
+    Input: 
+        X:  Data Matrix of interest (p by n) where n is the number of training samples and p is the number of features
+        v:  hyperparameter within (0,1)
+        K:  kernel, the default is a radius-based Gaussian kernel
+
+    Output: 
+        y:  labels predicting anomalous or not for training data
+        f:  classifier to predict labels for future data
+    '''
+
+    n = X.shape[1]
+    # TODO: minimize the dual problem
+
+
+
+    # classification
+    rho = 0 #TODO
+    f = lambda K, alpha, rho: 0 #TODO
+    y = 0 #TODO
+    return y, f
