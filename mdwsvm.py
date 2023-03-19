cf0eff40f5abe70acef01c7740ce635d8542a761
ashlynzzzz <kxinzhao@umich.edu>
2023-03-19 14:08:27 -0400

    Create mdwsvm.py

---
+++ mdwsvm.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cvxpy as cp
+
+def mdwsvm(X, W, alp = 0.5):
+    '''
+    This function aims to reimplement MDWSVM to do multiclass classification
+
+    Input: 
+        X:      Data Matrix of interest (p by n) where n is the number of training samples and p is the number of features
+        W:      vertices matrix for all classes
+        alp:    weighting parameter, the default value of 0.5
+        
+    Output: 
+        y:  labels for training data
+        f:  f(x) = x * B + beta_0 AMSVM decision boundary
+        f0: f0(x) = x * B + beta_d DWD decision boundary
+        classifier: argmax_j <f(x), W_j> MDWSVM classifier for predicting future data
+    '''
+    #TODO: minimize the dual problem
+    # input: X, W
+    # intermediate: r, ita, xi
+    # output: B, beta_0, beta_d
+
+    # classification
+    f = lambda x: 0 #TODO
+    f0 = lambda x: 0 #TODO
+    classifier = lambda x: 0 #TODO
+    y = classifier(X)
+    return y, f, f0, classifier
