a668b1ce996b07f269f9dc1ef46a492133de73c3
cindy-shaw <118010346@link.cuhk.edu.cn>
2023-03-20 18:15:55 -0400

    Update mdwd.py

--- mdwd.py
+++ mdwd.py
@@ -4,55 +4,52 @@ import cvxpy as cp
 class mdwd:
     '''
     This class aims to reimplement MDWD to do multiclass classification
+
+    Variables: 
+        X:      training data matrix of interest (d by n) where n is the number of training samples and d is the number of features
+        y:      training labels
+        W:      vertices matrix for all classes
+        C:      constraint hyperparameter on B which is the coefficient in f
     '''
 
-    def fit(self, X, y, W, C):
-        '''
-        This function aims to train MDWD
-
-        Input: 
-            X:  training data matrix of interest (d by n) where n is the number of training samples and d is the number of features
-            y:  training labels, should be an ndarray
-            W:  vertices matrix for all classes
-            C:  constraint hyperparameter on B which is the coefficient in f
-        '''
-
+    def __init__(self, X, y, W, C):
+        self.X = X
+        self.y = y
         self.W = W
-        d, n = X.shape
-        k = len(np.unique(y))
-        W_y = W[:,y]
+        self.C = C
+        self.B, self.beta = self.fit()
+
+    def fit(self):
+        d, n = self.X.shape
+        k = len(np.unique(self.y))
+        W_y = self.W[:,self.y]
 
         B = cp.Variable((d, k-1))
         beta = cp.Variable((k-1, 1))
         r = cp.Variable(n)
         eta = cp.Variable(n)
 
-        # Objective function for MSVM
+        # Objective function
         objective = cp.Minimize(cp.sum(cp.power(r, -1) + eta))
         # Constraints
-        constraints = [r == cp.diag((B.T @ X + beta).T @ W_y) + eta,
+        constraints = [r == cp.diag((B.T @ self.X + beta).T @ W_y) + eta,
                        r >= 0,
                        eta >= 0,
-                       cp.sum([cp.power(cp.pnorm(B[:,i], p=2), 2) for i in range(k-1)]) <= C]
+                       cp.sum([cp.power(cp.pnorm(B[:,i], p=2), 2) for i in range(k-1)]) <= self.C]
         prob = cp.Problem(objective, constraints)
         # prob.solve(solver=cp.ECOS)
         prob.solve()
 
-        self.B = B.value
-        self.beta = beta.value
+        return B.value, beta.value       
 
-        
-    def predict(self, X):
+    def predict(self, data):
         '''
-        This function aims to do prediction
-
         Input:
-            X:  data matrix for prediction (d by n)
+            data:   data matrix for prediction (d by n)
 
         Output:
-            y:  predicted labels
+            y:      predicted labels for data
         '''
-
-        f = self.B.T @ X + self.beta
+        f = self.B.T @ data + self.beta
         y = np.argmax(self.W.T @ f, axis=0)
         return y
