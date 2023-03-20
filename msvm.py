aa0bea13da35912dc107ccd53ec2060321a5ebda
cindy-shaw <118010346@link.cuhk.edu.cn>
2023-03-20 15:28:08 -0400

    Update msvm.py

--- msvm.py
+++ msvm.py
@@ -1,25 +1,58 @@
 import numpy as np
 import cvxpy as cp
 
-def msvm(X, W, C):
-    '''
-    This function aims to reimplement MSVM to do multiclass classification
 
-    Input: 
-        X:  Data Matrix of interest (d by n) where n is the number of training samples and p is the number of features
-        W:  vertices matrix for all classes
-        C:  constraint hyperparameter on B which is the coefficient in f
-        
-    Output: 
-        y:  labels for training data
-        f:  argmax_j <f(x), W_j> MSVM classifier for predicting future data where f(x) = x * B + beta
+class msvm:
     '''
-    #TODO: minimization
-    # input: X, W, C
-    # intermediate: xi
-    # output: B, beta
-#
-    # classification
-    f = lambda x: 0 #TODO
-    y = f(X)
-    return y, f
+    This class aims to reimplement MSVM to do multiclass classification
+    '''
+
+    def fit(self, X, y, W, C):
+        '''
+        This function aims to train MSVM
+
+        Input: 
+            X:  training data matrix of interest (d by n) where n is the number of training samples and d is the number of features
+            y:  training labels, should be an ndarray
+            W:  vertices matrix for all classes
+            C:  constraint hyperparameter on B which is the coefficient in f
+        '''
+
+        #TODO: minimization
+        # input: X, y, W, C
+        # output: B, beta
+        self.W = W
+        d, n = X.shape
+        k = len(np.unique(y))
+        W_y = W[:,y]
+
+        B = cp.Variable((d, k-1))
+        beta = cp.Variable((k-1, 1))
+        xi = cp.Variable(n)
+
+        # Objective function for MSVM
+        objective = cp.Minimize(cp.sum(xi))
+        # Constraints
+        constraints = [cp.diag((B.T @ X + beta).T @ W_y) + xi >= 1,
+                       xi >= 0, 
+                       cp.sum(cp.diag(B.T @ B)) <= C]
+        prob = cp.Problem(objective, constraints)
+        prob.solve()
+
+        self.B = B.value
+        self.beta = beta.value
+
+    def predict(self, X):
+        '''
+        This function aims to do prediction
+
+        Input:
+            X:  data matrix for prediction (d by n)
+
+        Output:
+            y:  predicted labels
+        '''
+        # classification
+        f = self.B.T @ X + self.beta
+        y = np.argmax(self.W.T @ f, axis=0)
+        return y
