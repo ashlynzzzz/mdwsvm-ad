592330931a576fa823a053a9e56888d672a79afa
ashlynzzzz <kxinzhao@umich.edu>
2023-03-20 13:49:41 -0400

    Update mdwsvm.py

--- mdwsvm.py
+++ mdwsvm.py
@@ -1,30 +1,39 @@
 import numpy as np
 import cvxpy as cp
 
-def mdwsvm(X, W, C, alp = 0.5):
+class mdwsvm():
     '''
-    This function aims to reimplement MDWSVM to do multiclass classification
+    This class aims to reimplement MDWSVM to do multiclass classification
 
-    Input: 
-        X:      Data Matrix of interest (d by n) where n is the number of training samples and p is the number of features
+    Variables: 
+        X:      training data matrix of interest (d by n) where n is the number of training samples and p is the number of features
+        y:      training labels
         W:      vertices matrix for all classes
         C:      constraint hyperparameter on B which is the coefficient in f
         alp:    weighting parameter, the default value of 0.5
-        
-    Output: 
-        y:  labels for training data
-        f:  f(x) = x * B + beta_0 AMSVM decision boundary
-        f0: f0(x) = x * B + beta_d DWD decision boundary
-        classifier: argmax_j <f(x), W_j> MDWSVM classifier for predicting future data
     '''
-    #TODO: minimization
-    # input: X, W, C, alp
-    # intermediate: r, ita, xi
-    # output: B, beta_0, beta_d
+    def __init__(self, X, y, W, C, alp = 0.5):
+        self.X = X
+        self.y = y
+        self.W = W
+        self.C = C
+        self.alpha = alp
+        self.B, self.beta = self.train()
 
-    # classification
-    f = lambda x: 0 #TODO
-    f0 = lambda x: 0 #TODO
-    classifier = lambda x: 0 #TODO
-    y = classifier(X)
-    return y, f, f0, classifier
+    def train(self):
+        '''
+        Output:
+        B, beta:     for f(x) = B.T@x + beta
+        '''
+        # intermediate: r, ita, xi
+        return B, beta
+
+    def test(data):
+        '''
+        Input:
+        data:   data for evaluation
+
+        Output:
+        y:      predicting labels for data
+        '''
+        return y
