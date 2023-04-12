6769b4207701405573126669e3f33bf3e801c8ff
ashlynzzzz <kxinzhao@umich.edu>
2023-04-12 13:11:37 -0400

    Update mdwsvm_ad.py

--- mdwsvm_ad.py
+++ mdwsvm_ad.py
@@ -6,7 +6,7 @@ class mdwsvm_ad:
     This class aims to reimplement MDWSVM to do multiclass classification
 
     Variables: 
-            X:      training data matrix of interest (d by n) where n is the number of training samples and dw is the number of features
+            X:      training data matrix of interest (d by n) where n is the number of training samples and d is the number of features
             y:      training labels
             W:      vertices matrix for all classes: K by (K+1)
             C:      constraint hyperparameter on B which is the coefficient in f
@@ -15,10 +15,11 @@ class mdwsvm_ad:
             K:      kernel function defined in matrix form, the default is a radius-based Gaussian kernel
     '''
 
-    def __init__(self, X, y, W, C, v, K, alp = 0.5):
+    def __init__(self, X, y, W, U, C, v, K, alp = 0.5):
         self.X = X
         self.y = y
         self.W = W
+        self.U = U
         self.C = C
         self.alp = alp
         self.v = v
@@ -26,12 +27,11 @@ class mdwsvm_ad:
         self.d, self.e = self.fit()
 
     def fit(self):
-        # TODO
         _, n = self.X.shape
-        k, _ = self.W.shape
+        self.k, _ = self.W.shape
         W_y = self.W[:,self.y]
         _, counts = np.unique(self.y, return_counts=True)
-        N_y = counts[self.y]
+        self.N_y = counts[self.y]
         # Calculate the train-train kernel matrix.
         G = self.K(self.X, self.X)
 
@@ -39,19 +39,19 @@ class mdwsvm_ad:
         e = cp.Variable(n)
         f = cp.Variable(n)
 
-        objective = cp.Minimize(0.5 * cp.sum([cp.quad_form(cp.multiply((d+e), W_y[j]), G) for j in range(k)])
-                                -2 * cp.sum(cp.sqrt(self.C*self.alp*(d-f) / (self.v*N_y))))
+        objective = cp.Minimize(0.5 * cp.sum([cp.quad_form(cp.multiply((d+e), W_y[j]), G) for j in range(self.k)])
+                                -2 * cp.sum(cp.sqrt(self.C*self.alp*(d-f) / (self.v*self.N_y))))
         
-        constraints = [W_y @ d == 0,
-                       W_y @ e == 0,
-                       cp.sum(e) == self.C,
-                       # d - f > 0,
+        constraints = [W_y @ d + (self.C - cp.sum(d))*self.W[:,-1] == 0,
+                       W_y @ e + (self.C - cp.sum(e))*self.W[:,-1] == 0,
+                       e <= self.C * (1-self.alp) / (self.v * self.N_y),
+                       d <= self.C * self.alp / (self.v * self.N_y),
                        d - f >= 1e-16, 
                        e >= 0,
                        f >= 0]
         
         prob = cp.Problem(objective, constraints)
-        prob.solve(solver=cp.SCS, verbose=1)
+        prob.solve(solver=cp.SCS, verbose=True)
 
         return d.value, e.value        
 
@@ -63,13 +63,21 @@ class mdwsvm_ad:
         Output:
             y:  predicted labels for data
         '''
-        # TODO
         # Compute the train-test kernel matrix
         G = self.K(self.X, data)
 
         g = self.d + self.e
         W = self.W
         W_y = self.W[:,self.y]
-        y = np.argmax(W.T @ ((np.diag(g) @ W_y.T).T @ G), axis=0)
+
+        constant = np.zeros(self.k)
+        index = np.where((self.e > 0) & (self.e < (1-self.alp)*self.C/(self.v*self.N_y)))[0]
+        for i in range(self.k):
+            selected_index = index[np.argmax(self.y[index] == i)]
+            selected_G = self.K(self.X, self.X[:,selected_index].reshape(-1,1))
+            constant[i] = - W[:,i].T @ (np.diag(g) @ W_y.T).T @ selected_G
+
+        constant = np.concatenate([constant,[0]]).reshape((-1,1))
+        y = np.argmax(W.T @ ((np.diag(g) @ W_y.T).T @ G) + constant, axis=0)
 
         return y
